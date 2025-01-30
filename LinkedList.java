41b47bcca5bb52c33aa38e1249d9d309bd3cbbca
asafaloush <asafaloush@gmail.com>
2025-01-30 18:07:09 +0200

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -155,11 +155,12 @@ public class LinkedList {
 	 *         if index is negative or greater than or equal to size
 	 */
 	public MemoryBlock getBlock(int index) {
-		if (index < 0 || index > size) {
+		if (index < 0 || index >= size) {
 			throw new IllegalArgumentException(
 					"index must be between 0 and size");
 		}
 		Node currentNode = getNode (index);
+		
 		return currentNode.block;
 	}	
 
